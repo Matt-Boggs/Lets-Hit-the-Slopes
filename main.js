bcf9ea5df7b4ed43306f36a3b8450f383e6b3989
Casino Apte <casino.apte@gmail.com>
2020-10-06 13:22:01 -0600

    Console logged the data

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 // 1 epic js
-  //vail
-  //breckenridge
-  //beaver creek
-  //keystone
-  //crested butte
-  //telluride
-  //snowmass
+//vail
+//breckenridge
+//beaver creek
+//keystone
+//crested butte
+//telluride
+//snowmass
 
 //1 icon js
-  //steamboat
-  //winter park
-  //copper mountain
-  //summit county
-  //aspen
-  //durango
+//steamboat
+//winter park
+//copper mountain
+//summit county
+//aspen
+//durango
 
 // Pseudo-coding to begin 
 
@@ -23,23 +23,60 @@
 
 //variables to create from API
 //resorts
-    //Report Date
-    // Percentage open
-    // Date of Last Significant snowfall
-    // Amount of last significant snowfall
-    // Consolidated snow depth on lower slopes
-    // Consolidated snow depth on upper slopes
-    // Surface condition description
-    // possibly!!! contact for resorts, address etc
+//Report Date
+// Percentage open
+// Date of Last Significant snowfall
+// Amount of last significant snowfall
+// Consolidated snow depth on lower slopes
+// Consolidated snow depth on upper slopes
+// Surface condition description
+// possibly!!! contact for resorts, address etc
 //weather
-    // cloud
-    // precipitation (rain, hail, snow, sleet)
-    // humidity
-    // visibility
-    // weather code, icon and description
-    // fresh snow
-    // temperature
-    // feels like temperature
-    // wind direction
-    // wind speed
-    // wind gust
+// cloud
+// precipitation (rain, hail, snow, sleet)
+// humidity
+// visibility
+// weather code, icon and description
+// fresh snow
+// temperature
+// feels like temperature
+// wind direction
+// wind speed
+// wind gust
+
+
+$(document).ready(function () {
+
+  var currWeatherPila = "https://api.weatherunlocked.com/api/resortforecast/666020?app_id=8ef92fdb&app_key=f1a993da508ddcdd9c8459ab494bbc83"
+  $.ajax({
+    url: currWeatherPila,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response)
+
+    // BASE SNOW STATS
+
+    console.log(response.forecast[0].base.freshsnow_in)
+    console.log(response.forecast[0].base.windspd_mph)
+    console.log(response.forecast[0].base.temp_f)
+    console.log(response.forecast[0].base.feelslike_f)
+    console.log(response.forecast[0].base.wx_desc)
+
+    // PEAK SNOW STATS
+
+    console.log(response.forecast[0].upper.freshsnow_in)
+    console.log(response.forecast[0].upper.windspd_mph)
+    console.log(response.forecast[0].upper.temp_f)
+    console.log(response.forecast[0].upper.feelslike_f)
+    console.log(response.forecast[0].upper.wx_desc)
+
+
+  });
+
+
+
+
+
+})
+
+
