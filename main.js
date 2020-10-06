4874c839ade033dc73d32b6ff83758948b247c42
Taylor <taylor.hackbart@du.edu>
2020-10-06 10:43:56 -0600

    html

---
+++ main.js
@@ -0,0 +1,45 @@
+// 1 epic js
+  //vail
+  //breckenridge
+  //beaver creek
+  //keystone
+  //crested butte
+  //telluride
+  //snowmass
+
+//1 icon js
+  //steamboat
+  //winter park
+  //copper mountain
+  //summit county
+  //aspen
+  //durango
+
+// Pseudo-coding to begin 
+
+//2 function, 
+// 1 for resort
+// 1 for weather conditions 
+
+//variables to create from API
+//resorts
+    //Report Date
+    // Percentage open
+    // Date of Last Significant snowfall
+    // Amount of last significant snowfall
+    // Consolidated snow depth on lower slopes
+    // Consolidated snow depth on upper slopes
+    // Surface condition description
+    // possibly!!! contact for resorts, address etc
+//weather
+    // cloud
+    // precipitation (rain, hail, snow, sleet)
+    // humidity
+    // visibility
+    // weather code, icon and description
+    // fresh snow
+    // temperature
+    // feels like temperature
+    // wind direction
+    // wind speed
+    // wind gust
